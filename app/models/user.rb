76c7a83cfe1eb358fd2d89a6059591488a64116e
Alfred Tuva <muindetuva@gmail.com>
2025-02-16 19:09:05 +0300

    feat: add email validation and tests to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
   has_many :borrowed_books, through: :borrowing_records, source: :book
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  validates :email_address, presence: true, uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not save user without email" do
+    user = User.new(password: "password")
+    assert_not user.valid?, "User can't be saved without an email"
+  end
+
+  test "should not save user without password" do
+    user = User.new(email_address: "test@test.com")
+    assert_not user.valid?, "User can't be saved without a password"
+  end
+
+  test "should not allow duplicate email_address" do
+    User.create!(email_address: "test@test.com", password: "password")
+    duplicate_user = User.new(email_address: "test@test.com", password: "password")
+
+    assert_not duplicate_user.valid?, "Email has to be unique"
+  end
+
 end
