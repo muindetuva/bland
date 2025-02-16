b5ad7633275108a67e47e94febb5a2849f9b16d4
Alfred Tuva <muindetuva@gmail.com>
2025-02-16 18:43:01 +0300

    feat: add borrowing associations to User and Book models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,7 @@
 class Book < ApplicationRecord
+  has_many :borrowing_records, dependent: :destroy
+  has_many :users, through: :borrowing_records
+
   validates :title, presence: true
   validates :author, presence: true
   validates :isbn, presence: true, uniqueness: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
+  has_many :borrowing_records, dependent: :destroy
+  has_many :borrowed_books, through: :borrowing_records, source: :book
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
