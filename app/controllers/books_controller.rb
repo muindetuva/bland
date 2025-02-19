aa69a90f28593d5306a2316c3f9ecbe5d6331f69
Alfred Tuva <muindetuva@gmail.com>
2025-02-19 20:35:19 +0300

    feat: add filtering on the books index action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,18 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
+
+    @filter = params[:filter] || "all"
+
+    @books = case @filter
+    when "available"
+      Book.where.not(id: BorrowingRecord.where(returned_at: nil).select(:book_id))
+    when "borrowed"
+      Book.where(id: BorrowingRecord.where(returned_at: nil).select(:book_id))
+    else
+      Book.all
+    end
   end
+
 
   def show
     @book = Book.find(params[:id])
